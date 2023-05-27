30aa33f0e414f75ce6fac37a4d21e0ab7d433183
Ezequiel Parziale <eparziale@gmail.com>
2023-05-26 22:09:36 -0300

    :wrench: update: change default values env vars

--- src/configs/general.config.js
+++ src/configs/general.config.js
@@ -5,16 +5,16 @@ const PORT = process.env.PORT || 3000
 const BACKEND_CORS_ORIGINS = process.env.BACKEND_CORS_ORIGINS || '*'
 
 // Jwt
-const SECRET_KEY = process.env.SECRET_KEY || 3000
-const ALGORITHM = process.env.ALGORITHM || 3000
-const ACCESS_TOKEN_EXPIRE_MINUTES = process.env.ACCESS_TOKEN_EXPIRE_MINUTES || 3000
+const SECRET_KEY = process.env.SECRET_KEY
+const ALGORITHM = process.env.ALGORITHM || 'HS256'
+const ACCESS_TOKEN_EXPIRE_MINUTES = process.env.ACCESS_TOKEN_EXPIRE_MINUTES || 30
 
 // Postgres Database
-const POSTGRES_HOSTNAME = process.env.POSTGRES_HOSTNAME || 3000
-const POSTGRES_PORT = process.env.POSTGRES_PORT || 3000
-const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || 3000
-const POSTGRES_USER = process.env.POSTGRES_USER || 3000
-const POSTGRES_DB = process.env.POSTGRES_DB || 3000
+const POSTGRES_HOSTNAME = process.env.POSTGRES_HOSTNAME
+const POSTGRES_PORT = process.env.POSTGRES_PORT || 5432
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
+const POSTGRES_USER = process.env.POSTGRES_USER
+const POSTGRES_DB = process.env.POSTGRES_DB
 
 module.exports = {
   PORT,
